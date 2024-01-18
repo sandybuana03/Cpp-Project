5b15513f63a913857a533045ee9361f4d54f163a
sandybuana03 <sandybuana03@gmail.com>
2024-01-18 08:06:47 +0700

    Create truefalse.cpp

---
+++ truefalse.cpp
@@ -0,0 +1,15 @@
+// penyajian nilai true dan false
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "NILAI True dan False" << endl;
+	cout << "====================" << endl;
+	
+	cout << "Nilai True  = " << true << endl;
+	cout << "Nilai False = " << false << endl;
+	
+	return(0);
+}
